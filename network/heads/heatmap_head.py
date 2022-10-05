11956363c3250f8b0973abca7365c96d79f62e14
PSRahul <rahulpsr95@gmail.com>
2022-10-05 09:30:14 +0200

    add orginal heatmap processing

--- network/heads/heatmap_head.py
+++ network/heads/heatmap_head.py
@@ -44,15 +44,17 @@ class SMP_HeatMapHead(nn.Module):
 
     def forward(self, x):
         x = self.model.forward(x)
-        x_like = torch.zeros_like(x)
-        y = torch.max(x.view(x.shape[0], -1), dim=1)[0]
-        for i in range(x.shape[0]):
-             if (y[i] < 1e-3):
-                 y[i] = 1e-3
+        return x
+        #x_like = torch.zeros_like(x)
+        #y = torch.max(x.view(x.shape[0], -1), dim=1)[0]
+        #for i in range(x.shape[0]):
+        #    x_like[i] = y[i]
 
-             x_like[i] = y[i]
+#            if (y[i] < 1e-3):
+ #                y[i] = 1e-3
 
-        return x / x_like
+
+        #return x / x_like
 
     def print_details(self):
         batch_size = 32
